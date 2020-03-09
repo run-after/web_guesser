53fce910a51cbea515622801f6268229ea8fde43
run-after <run.after@protonmail.com>
2020-03-08 19:15:53 -0500

    I1, generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/reloader'
+enable :sessions
 
 get '/' do
-  "Hello, World!"
+  session[:value] = rand(100)
+  redirect "/45"
 end
+
+get "/:value" do
+  "The secret number is " << session[:value].inspect
+end
+
+
+
+
