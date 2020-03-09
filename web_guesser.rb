06c58c286b6149cfd8e2a272a8dedc9bea46f43d
run-after <run.after@protonmail.com>
2020-03-08 19:59:48 -0500

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,14 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 enable :sessions
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess']
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
-
+def check_guess(guess)
+if params['guess'].to_i > SECRET_NUMBER
+  if params['guess'].to_i > SECRET_NUMBER + 5
+    message = 'Way too high!'
+  else 
+    message = 'Too high!'
+  end
+elsif params['guess'].to_i < SECRET_NUMBER
+  if params['guess'].to_i < SECRET_NUMBER - 5
+    message = 'Way too low!'
+  else
+    message = "Too low!"
+  end
+else
+  message = "You got it right! The SECRET NUMBER is: #{SECRET_NUMBER}"
+end
+end
 
 
 
