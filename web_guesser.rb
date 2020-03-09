40fd69f105987538f5fd7598500c6033826c458e
run-after <run.after@protonmail.com>
2020-03-09 09:42:37 -0500

    I4: change background based on message

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
-enable :sessions
-SECRET_NUMBER = rand(100)
 
+SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params['guess']
@@ -11,22 +10,19 @@ get '/' do
 end
 
 def check_guess(guess)
-if params['guess'].to_i > SECRET_NUMBER
-  if params['guess'].to_i > SECRET_NUMBER + 5
-    message = 'Way too high!'
-  else 
-    message = 'Too high!'
-  end
-elsif params['guess'].to_i < SECRET_NUMBER
-  if params['guess'].to_i < SECRET_NUMBER - 5
-    message = 'Way too low!'
+  if params['guess'].to_i > SECRET_NUMBER
+   if params['guess'].to_i > SECRET_NUMBER + 5
+     'Way too high!'
+    else 
+      'Too high!'
+    end
+  elsif params['guess'].to_i < SECRET_NUMBER
+    if params['guess'].to_i < SECRET_NUMBER - 5
+      'Way too low!'
+    else
+      "Too low!"
+    end
   else
-    message = "Too low!"
+    message = "You got it right! The SECRET NUMBER is: #{SECRET_NUMBER}"
   end
-else
-  message = "You got it right! The SECRET NUMBER is: #{SECRET_NUMBER}"
 end
-end
-
-
-
