a9ec7f05585e6caccc326febe698b5bdd94b59de
run-after <run.after@protonmail.com>
2020-03-09 09:52:10 -0500

    I4: correct start of game to read instructions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,28 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+  SECRET_NUMBER = rand(100)
 
-get '/' do
-  guess = params['guess']
-  message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
-end
-
-def check_guess(guess)
-  if params['guess'].to_i > SECRET_NUMBER
-   if params['guess'].to_i > SECRET_NUMBER + 5
-     'Way too high!'
-    else 
-      'Too high!'
-    end
-  elsif params['guess'].to_i < SECRET_NUMBER
-    if params['guess'].to_i < SECRET_NUMBER - 5
-      'Way too low!'
-    else
-      "Too low!"
-    end
-  else
-    message = "You got it right! The SECRET NUMBER is: #{SECRET_NUMBER}"
+  get '/' do
+    guess = params['guess']
+    message = check_guess(guess)
+    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
   end
-end
+
+  def check_guess(guess)
+    if params['guess'] == nil
+      'Guess a number between 0 - 100'
+    elsif params['guess'].to_i > SECRET_NUMBER
+      if params['guess'].to_i > SECRET_NUMBER + 5
+       'Way too high!'
+      else 
+        'Too high!'
+       end
+    elsif params['guess'].to_i < SECRET_NUMBER
+      if params['guess'].to_i < SECRET_NUMBER - 5
+        'Way too low!'
+      else
+        "Too low!"
+      end
+    else
+      message = "You got it right! The SECRET NUMBER is: #{SECRET_NUMBER}"
+    end
+  end
