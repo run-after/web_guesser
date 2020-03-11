1a8ce5dcf175254b6431161e7e69d2337794a83e
run-after <run.after@protonmail.com>
2020-03-11 18:33:02 -0500

    I4: correcct guess limiting so that you don't get an extra turn when restarting game ... also finished cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,52 +1,49 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-
-  SECRET_NUMBER = rand(100)
+  @@secret_number = rand(100)
   @@turns = 5
 
   get '/' do
     if @@turns < 0
       reload_game
     end
-    cheat = 
+    cheat = params['cheat']
     guess = params['guess']
     message = check_guess(guess)
-    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+    erb :index, :locals => {:number => @@secret_number, :message => message, :cheat => cheat}
   end
 
   def check_guess(guess)
-  if @@turns > 0 
+  if @@turns > 0
     @@turns -= 1
     if params['guess'] == nil
       'Guess a number between 0 - 100'
-    elsif params['guess'].to_i > SECRET_NUMBER
-      if params['guess'].to_i > SECRET_NUMBER + 5
+    elsif params['guess'].to_i > @@secret_number
+      if params['guess'].to_i > @@secret_number + 5
        'Way too high!'
       else 
         'Too high!'
        end
-    elsif params['guess'].to_i < SECRET_NUMBER
-      if params['guess'].to_i < SECRET_NUMBER - 5
+    elsif params['guess'].to_i < @@secret_number
+      if params['guess'].to_i < @@secret_number - 5
         'Way too low!'
       else
         "Too low!"
       end
     else 
       @@turns = -1
-    "You got it right! The SECRET NUMBER was: #{SECRET_NUMBER} New number loaded!"  
+    "You got it right! The SECRET NUMBER was: #{@@secret_number} New number loaded!"  
     end
   else
-    if params['guess'].to_i != SECRET_NUMBER
+    if params['guess'].to_i != @@secret_number
     @@turns = -1
-    "Sorry... The number was #{SECRET_NUMBER}. Number reloaded, try again if you'd like"
-    else
-      @@turns = -1
-      "You're right... The number was #{SECRET_NUMBER}. Number reloaded, try again if you'd like"
+    "Sorry... The number was #{@@secret_number}. Number reloaded, try again if you'd like"
     end
   end
 end
 
 def reload_game
-  load './web_guesser.rb'
+  @@turns = 4
+  @@secret_number = rand(100)
 end
